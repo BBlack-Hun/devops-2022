7b60468b79be0d21faf54e7fe7e2c13e5b266a55
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-06-22 14:26:07 +0900

    test ci/cd2

--- src/main/java/devops/controller/HomeController.java
+++ src/main/java/devops/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping
     public String home() {
-        return "빌드가 자동으로 되는지 테스트111111111111111111111";
+        return "빌드가 자동으로 되는지 테스트111111111111111111111</br>이제 배포도 되는가???";
     }
 }
